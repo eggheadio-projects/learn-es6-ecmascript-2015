c5079c8b8c612658850564f2597290fc871d4abe
lsminter <lsminteris@gmail.com>
2017-10-02 12:43:14 -0700

    prepare plunker

--- ecmascript-6-converting-an-array-like-object-into-an-array-with-array-from/script.js
+++
@@ -1,6 +0,0 @@
-const products =
-  Array.from(document.querySelectorAll('.product'));
-
-products
-  .filter(product => parseFloat(product.innerHTML) < 10)
-  .forEach(product => product.style.color = 'red');
